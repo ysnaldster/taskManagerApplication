bee133913a46e1733d418677d61eeb15125ccbb8
ysnaldster <ysnaldolopez@gmail.com>
2022-08-28 20:05:13 -0500

    Update with database connection Postgres

--- Program.cs
+++ Program.cs
@@ -3,15 +3,22 @@ using Microsoft.EntityFrameworkCore;
 using TaskManagerApplication.Context;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TaskManagerApplicationContext>(p => p.UseInMemoryDatabase("taskManagerDb"));
+//builder.Services.AddDbContext<TaskManagerApplicationContext>(p => 
+    //p.UseInMemoryDatabase("taskManagerDb"));
+builder.Services.AddNpgsql<TaskManagerApplicationContext>
+    (builder.Configuration.GetConnectionString("task_manager_db"));
+//builder.Services.AddSqlServer<TaskManagerApplicationContext>("Data Source=X;" +
+                                                             //"Initial Catalog=X;user id=sa;password=dominio ");
 
 var app = builder.Build();
 
+
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/dbconnection", async ([FromServices] TaskManagerApplicationContext dbContext) =>
+app.MapGet("/dbconnection", async ([FromServices] 
+    TaskManagerApplicationContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
-    return Results.Ok("Database in memory" + dbContext.Database.IsInMemory());
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
 });
 
 app.Run();
