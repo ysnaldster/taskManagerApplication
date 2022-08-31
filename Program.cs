94aec0cf99f14da0ea859bd4a860e98f5f2ae75d
ysnaldster <ysnaldolopez@gmail.com>
2022-08-30 20:54:57 -0500

    Changes doesn't fluent API

--- Program.cs
+++ Program.cs
@@ -7,12 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
     //p.UseInMemoryDatabase("taskManagerDb"));
 builder.Services.AddNpgsql<TaskManagerApplicationContext>
     (builder.Configuration.GetConnectionString("task_manager_db"));
-//builder.Services.AddSqlServer<TaskManagerApplicationContext>("Data Source=X;" +
-                                                             //"Initial Catalog=X;user id=sa;password=dominio ");
+//builder.Services.AddSqlServer<TaskManagerApplicationContext>(builder.Configuration.GetConnectionString("task_manager_db")));
 
 var app = builder.Build();
-
-
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/dbconnection", async ([FromServices] 
     TaskManagerApplicationContext dbContext) =>
